33aa8e52f1aba06cb1a625cdb1581005355ae5d6
JuniorEscoto <juniorjescoto@gmail.com>
2018-11-30 08:31:01 -0600

    grid small change

--- src/app/components/search-bar/search-bar.component.ts
+++ src/app/components/search-bar/search-bar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-search-bar',
@@ -9,14 +10,31 @@ import { FormControl } from '@angular/forms';
 export class SearchBarComponent implements OnInit {
   public showTable;
   myControl = new FormControl();
-  options: string[] = ['taladro', 'taladro acero', 'Taladro bronce'];
-  constructor() { }
+  options: string[] = [];
+  constructor(private http: HttpClient) { }
   public value;
   ngOnInit() {
     this.showTable = false;
+    this.httpGet('product');
   }
 
   getPosts(value) {
     this.value = value;
+  }
+
+  httpGet(url) {
+    this.http
+    .get('http://192.168.137.121/api/' + url)
+    .subscribe(data => {
+      if (data) {
+        this.convertData(data);
+      }
+    });
+  }
+
+  convertData(data) {
+    data.forEach(element => {
+      this.options.push(element.nombre);
+    });
   }
 }
