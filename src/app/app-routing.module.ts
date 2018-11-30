c299cdf9dc807fd391d757e0d70a24dce9601184
Norman Aráuz <netman.arauz@gmail.com>
2018-11-30 03:51:00 -0600

    Resolve errors

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,23 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { SearchBarComponent } from './components/search-bar/search-bar.component';
-<<<<<<< HEAD
 import { GridComponent } from './components/grid/grid.component';
-
-const routes: Routes = [
-  { path: 'search-bar', component: SearchBarComponent },
-  { path: 'grid', component: GridComponent }
-=======
 import { PerfilTiendaComponent } from './components/perfil-tienda/perfil-tienda.component';
-import {LoginComponent} from './components/login/login.component';
+import { LoginComponent } from './components/login/login.component';
 import { TabsComponent } from './components/tabs/tabs.component';
 
+
 const routes: Routes = [
   { path: 'search-bar', component: SearchBarComponent },
+  { path: 'grid', component: GridComponent },
   { path: 'perfil-tienda', component: PerfilTiendaComponent },
-  {path: 'login', component: LoginComponent}
+  { path: 'login', component: LoginComponent },
   { path: 'tabs', component: TabsComponent }
->>>>>>> c4476638cdef02f931ce20cf08a02fb2930b53f9
 ];
 
 @NgModule({
