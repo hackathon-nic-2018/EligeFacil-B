e49c50370816ac8ded131724c7e8af6938cd6636
Norman Aráuz <netman.arauz@gmail.com>
2018-11-30 01:02:25 -0600

    Grid added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { MaterialModule } from './material.module';
+import { AgGridModule } from 'ag-grid-angular';
+import { HttpModule } from '@angular/http';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { SearchBarComponent } from './components/search-bar/search-bar.component';
 import { PerfilTiendaComponent } from './components/perfil-tienda/perfil-tienda.component';
 import { TabsComponent } from './components/tabs/tabs.component';
+import { ListComponentComponent } from './components/list-component/list-component.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SearchBarComponent,
     PerfilTiendaComponent,
-    TabsComponent
+    TabsComponent,
+    ListComponentComponent
   ],
   imports: [
     BrowserModule,
     MaterialModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AgGridModule.withComponents([]),
+    HttpModule,
+    HttpClientModule
   ],
   providers: [],
+  schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
