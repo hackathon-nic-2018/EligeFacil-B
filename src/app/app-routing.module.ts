0dc0504067319c131454b579d4ed53086664494d
Norman Aráuz <netman.arauz@gmail.com>
2018-11-30 03:42:55 -0600

    Grid added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { SearchBarComponent } from './components/search-bar/search-bar.component';
+import { GridComponent } from './components/grid/grid.component';
 
 const routes: Routes = [
-  { path: 'search-bar', component: SearchBarComponent }
+  { path: 'search-bar', component: SearchBarComponent },
+  { path: 'grid', component: GridComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { MaterialModule } from './material.module';
+import { AgGridModule } from 'ag-grid-angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { SearchBarComponent } from './components/search-bar/search-bar.component';
+import { GridComponent } from './components/grid/grid.component';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    SearchBarComponent
+    SearchBarComponent,
+    GridComponent
   ],
   imports: [
     BrowserModule,
     MaterialModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AgGridModule.withComponents([]),
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
